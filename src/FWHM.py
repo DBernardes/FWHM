a128ecbd48c34dad0d615c81907c662c06511740
Denis <denis.bernardes099@gmail.com>
2020-10-23 09:36:41 -0300

    added image with the star radius

--- src/FWHM.py
+++ src/FWHM.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# Esta biblioteca contem a classe que calcula a relacao sinal-ruido de uma das imagens do janderson,
-# assim como o fluxo de fotons da estrela e do ceu.
-#Denis Varise Bernardes.
+#ahuthor:Denis Varise Bernardes.
 #26/11/2019.
+
+'''
+This library contains the class of the calculation of the star FWHM. Initially, it is needed to provide to the class the name
+of the FITS image, the (x,y) corrdinates of the star, and the maximum sky radius. The star image will be read and the function
+get_max_count() will seek for the pixel within the sky radius with the maximum counts value. The function set_centroid() is
+optional and it resets the star centroid based on the maximum pixel value found in previous step. Then, the function
+calc_FWHM() will calculate the star FWHM. The star radius length will be three times the FWHM length.
+'''
 
 from math import exp, sqrt
 import astropy.io.fits as fits
@@ -16,62 +22,41 @@ from scipy.interpolate import UnivariateSpline
 
 class fwhm:
 
-    def __init__(self, img_name, bias_name, xy_star, sky_radius, exp_time = 0, ccd_gain = 0, ccd_serial = 9916, ccd_temp = -70):        
-        self.exp_time = exp_time
-        self.n_pixels_star = 1
-        self.sky_flux = 0
-        self.dark_noise = 0
-        self.read_noise = 0      
+    def __init__(self, img_name, xy_star, sky_radius, bias_name='', exp_time = 0, dark_current_noise = 0, ccd_gain = 0, em_gain = 0, ccd_serial = 9916, ccd_temp = -70):
+        #FWHM calculation
         self.img_name = img_name
         self.x = xy_star[0]
         self.y = xy_star[1]
-        self.sky_radius = sky_radius        
-        self.img_data = 0
-        self.sky_flux = 0                
-        self.star_flux = 0        
+        self.sky_radius = sky_radius
+        
+        #SNR calculation
+        self.dark_noise = dark_current_noise
         self.ccd_gain = ccd_gain
+        self.em_gain = em_gain
         self.ccd_serial = ccd_serial
         self.ccd_temp = ccd_temp
-        self.bias_data = 0
         self.bias_name = bias_name
-        self.fwhm = 0
+        self.exp_time = exp_time
+        
+        self.n_pixels_star = 0
+        self.sky_flux = 0        
+        self.read_noise = 0                    
+        self.img_data = 0
+        self.sky_flux = 0                
+        self.star_flux = 0             
+        self.bias_data = 0        
         self.max_star_flux = 0
+        self.fwhm = 0
         self.SNR = 0
 
 
         
-    def read_star_img(self):   
-
+    def read_star_img(self):  
         #read the star image
         self.img_data = fits.getdata(self.img_name).astype(float)
-        header = fits.getheader(self.img_name)
+        self.header = fits.getheader(self.img_name)
         img_shape = self.img_data.shape
-        if img_shape[0] == 1: self.img_data = self.img_data[0]       
-
-        #------------------------------------------------------------------------------        
-        
-        #equations of the dark current characterization of the SPARC4 CCDs
-        #Access https://github.com/DBernardes/FWHM.git for more details
-        ccd_temp = self.ccd_temp
-        if self.ccd_serial == 9914:
-            self.dark_noise = 24.66*exp(0.0015*ccd_temp**2+0.29*ccd_temp) 
-        if self.ccd_serial == 9915:
-            self.dark_noise = 35.26*exp(0.0019*ccd_temp**2+0.31*ccd_temp)
-        if self.ccd_serial == 9916:
-            self.dark_noise = 9.67*exp(0.0012*ccd_temp**2+0.25*ccd_temp)
-        if self.ccd_serial == 9917:
-            self.dark_noise = 5.92*exp(0.0005*ccd_temp**2+0.18*ccd_temp)
-
-        #------------------------------------------------------------------------------
-        #try to find the exposure time in the image header
-        if self.exp_time == 0:
-            try:
-                string_texp = header['exposure'].split(',')
-                string_texp = string_texp[0] + '.' + string_texp[1]        
-                self.exp_time = float(string_texp)
-            except:
-                string_texp = header['exposure']                 
-                self.exp_time = float(string_texp)       
+        if img_shape[0] == 1: self.img_data = self.img_data[0]            
            
         
 
@@ -97,8 +82,7 @@ class fwhm:
         #get all the pixels of the science image within the sky radius
         star_flux = self.img_data[np.where(mask)]
         #return the maximum value of the star
-        self.max_star_flux = max(star_flux)
-        #print(self.max_star_flux, self.sky_flux),exit()       
+        self.max_star_flux = max(star_flux)        
 
     
 
@@ -106,8 +90,7 @@ class fwhm:
         #return the coordenates of the pixel with the maximum value
         y_max, x_max = np.where(self.img_data==self.max_star_flux)
         #set the found coordenates as the new star coordenates
-        self.x, self.y  = x_max[0], y_max[0]
-        #print(self.x, self.y),exit()       
+        self.x, self.y  = x_max[0], y_max[0]        
         
          
 
@@ -128,8 +111,7 @@ class fwhm:
         #The FWHM will be the distance between the two roots found
         self.fwhm = r2-r1
         #the star radius will be 3*fwhm found
-        self.star_radius = 3*self.fwhm
-        #print(self.star_radius)       
+        self.star_radius = 3*self.fwhm        
         x,y = self.x+1, self.y+1
         return self.fwhm, self.star_radius, x, y
 
@@ -139,6 +121,50 @@ class fwhm:
         bias_shape = self.bias_data.shape        
         if bias_shape[0] == 1: self.bias_data = self.bias_data[0]
         self.read_noise = np.std(self.bias_data)*self.ccd_gain
+
+
+    def calc_dark_current(self):     
+        #equations of the dark current characterization of the SPARC4 CCDs
+        #Access https://github.com/DBernardes/FWHM.git for more details
+        if self.dark_noise == 0:
+            ccd_temp = self.ccd_temp
+            if self.ccd_serial == 9914:
+                self.dark_noise = 24.66*exp(0.0015*ccd_temp**2+0.29*ccd_temp) 
+            if self.ccd_serial == 9915:
+                self.dark_noise = 35.26*exp(0.0019*ccd_temp**2+0.31*ccd_temp)
+            if self.ccd_serial == 9916:
+                self.dark_noise = 9.67*exp(0.0012*ccd_temp**2+0.25*ccd_temp)
+            if self.ccd_serial == 9917:
+                self.dark_noise = 5.92*exp(0.0005*ccd_temp**2+0.18*ccd_temp)
+
+            
+
+    def read_exp_time(self):        
+        #try to find the exposure time in the image header
+        if self.exp_time == 0:
+            try:
+                string_texp = self.header['exposure'].split(',')
+                string_texp = string_texp[0] + '.' + string_texp[1]        
+                self.exp_time = float(string_texp)
+            except:
+                string_texp = self.header['exposure']                 
+                self.exp_time = float(string_texp)
+
+
+    def read_em_gain(self):        
+        #try to find the EM gain in the image header
+        if self.em_gain == 0:
+            self.em_gain = 1
+            try:
+                string_em_gain = self.header['EMGAIN'].split(',')
+                string_em_gain = string_texp[0] + '.' + string_texp[1]        
+                self.em_gain = float(string_texp)
+            except:1
+            try:
+                string_em_gain = self.header['EMGAIN']                 
+                self.em_gain = float(string_texp)
+            except:1
+                  
 
         
 
@@ -155,8 +181,7 @@ class fwhm:
         #create two indice arrays
         ym, xm = np.indices(img_shape, dtype='float32')        
         #create a array with the center in the (x,y) provided values
-        r = np.sqrt((xm - self.x)**2 + (ym - self.y)**2)
-        #cria uma máscara configurando como valor 1 os pixeis dentro dos dois cículos
+        r = np.sqrt((xm - self.x)**2 + (ym - self.y)**2)        
         #create a mask seeting as 1 those pixels within 2 star radius and 3 star radius
         mask = (r > 2* self.star_radius) * (r < 3 * self.star_radius) * working_mask
         #calculate the median of the pixels for the sku flux
@@ -167,41 +192,31 @@ class fwhm:
         #get the star flux and subtracts it from the sky flux
         self.star_flux = (self.img_data[np.where(mask)] - self.sky_flux).sum()
         #number of the star pixels
-        self.n_pixels_object = len(self.img_data[np.where(mask)])
+        self.n_pixels_star = len(self.img_data[np.where(mask)])
         
 
 
-    def calc_SNR(self):
-        #Esta funcao calcula o SNR da estrela
-        gain = self.gain
-        star = self.star_flux
+    def calc_SNR(self):        
+        gain = self.ccd_gain
+        star = self.star_flux*gain
         self.sky_flux -= np.median(self.bias_data)
-        sky = self.sky_flux
-        n_pix = self.n_pixels_object
-        dc = self.dark_noise
-        t_exp = self.exp_time
+        sky = self.sky_flux*gain
+        n_pix = self.n_pixels_star        
+        exp_time = self.exp_time
+        dc = self.dark_noise*exp_time
         rn = self.read_noise        
 
-        em_gain = 1 #testes da SNR com o emgain        
-        self.star_flux*=em_gain
-        self.sky_flux*=em_gain
-        self.dark_noise*=em_gain        
-        aux = np.sqrt(star*gain + n_pix * (sky*gain + dc*t_exp + rn**2))
-        self.SNR = star*gain/aux            
+        #if the CCD is a EMCCD, apply the em_gain
+        em_gain = self.em_gain 
+        star*= em_gain
+        sky *= em_gain
+        dc  *= em_gain
 
+        #SNR equation
+        aux = np.sqrt(star + n_pix * (sky + dc + rn**2))
+        self.SNR = star/aux
 
-    def get_results(self):
-        fwhm = self.fwhm
-        snr = self.SNR
-        n_pixels = self.n_pixels_star
-        t_exp = self.exp_time
-        dc = self.dark_noise*t_exp
-        rn = self.read_noise           
-        std_bias = 276.608517296231 #ADU        
-        sky_flux = self.ccd_gain * (self.sky_flux) / t_exp - dc        
-        star_flux = self.ccd_gain * self.star_flux / t_exp        
-        star_radius = self.star_radius
-        x,y = self.x+1, self.y+1 #precisa somar 1 por causa da diferença pro DS9
-        return snr, fwhm, dc, rn, sky_flux, star_flux, n_pixels, star_radius, x, y
+        return self.SNR, rn, sky/exp_time, star/exp_time, n_pix
+    
 
-
+   
--- src/run.py
+++ src/run.py
@@ -12,25 +12,30 @@ file_dir = r'C:\Users\observer\Desktop\FWHM\Sample images'
 file_name = 'star.fits'
 bias_name = 'bias.fits'
 
-FWHM_obj = FWHM.fwhm(img_name = file_dir + '\\' +  file_name,
-                     bias_name = file_dir + '\\' +  bias_name,
+FWHM_obj = FWHM.fwhm(img_name = file_dir + '\\' +  file_name,                     
                      xy_star = (507,369),
-                     sky_radius = 20)
+                     sky_radius = 20,
+                     bias_name = file_dir + '\\' +  bias_name,
+                     ccd_gain = 3.36,
+                     em_gain = 0)
 
 FWHM_obj.read_star_img()
 FWHM_obj.get_max_count()
 FWHM_obj.set_centroid()
 fwhm, star_radius, x, y = FWHM_obj.calc_FWHM()
-#FWHM_obj.read_bias_img()
-#FWHM_obj.calc_star_sky_flux()
-#FWHM_obj.calc_SNR()
-#snr, fwhm, dc, rn, sky_flux, star_flux, n_pixels, star_radius, x, y = FWHM_obj.get_results()
+
+FWHM_obj.read_bias_img()
+FWHM_obj.calc_dark_current()
+FWHM_obj.read_exp_time()
+FWHM_obj.read_em_gain()
+FWHM_obj.calc_star_sky_flux()
+snr, rn, sky_flux, star_flux, n_pixels = FWHM_obj.calc_SNR()
 
 print('\n')
 print('FWHM: ', fwhm)
 print('Star Radius:', star_radius)
 print('Centroide: %i,%i'%(x,y))
-##print('SNR: ', snr)
-##print('Sky Flux: ', sky_flux, 'photons/s')
-##print('Star Flux:', star_flux, 'photons/s')
-#print('Star Pixels:', n_pixels)
+print('SNR: ', snr)
+print('Sky Flux: ', sky_flux, 'photons/s')
+print('Star Flux:', star_flux, 'photons/s')
+print('Star Pixels:', n_pixels)
